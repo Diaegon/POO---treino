c4ad39650fb7ff0f364b7acdac2887e0f4858f82
Diêgo Canafistula <diego.canafistula@gmail.com>
2025-11-24 14:21:29 -0300

    projeto01 estruturado finalizado

---
+++ proj01-OO.py
@@ -0,0 +1,2 @@
+class RetornaLista():
+    pass
--- proj01.py
+++ proj01.py
@@ -8,29 +8,51 @@
 
 #primeira parte
 
-entrada = input("Digite 5 números quaisquer entre 0 - 99, separados por virgula")
-entrada = entrada.split(",")
-saida = []
 
-while len(entrada) != 5:
+
+def check_numeros(tupla_valores):
+    for item in tupla_valores:
+        if int(item) > 99:
+            print("Entrada inválida: todos os números devem estar no intervalo de 0 a 99. Tente novamente.")
+            return False
+    for item in tupla_valores:
+        if int(item) < 0:
+            print("Entrada inválida: todos os números devem estar no intervalo de 0 a 99. Tente novamente.")
+            return False
+    return True
+
+def transforma_inteiro(tupla_valores):
+    lista_final = []
+    for item in tupla_valores:
+        lista_final.append(int(item))
+    return lista_final
+
+def check_tamanho():
     entrada = input("Digite 5 números quaisquer entre 0 - 99, separados por virgula")
     entrada = entrada.split(",")
+    while len(entrada) != 5:
+        entrada = input("Digite 5 números quaisquer entre 0 - 99, separados por virgula")
+        entrada = entrada.split(",")
+        tupla_valores = tuple(entrada)
+        checagem = check_numeros(tupla_valores)
+        if checagem == True and len(entrada) == 5:
+            saida = transforma_inteiro(tupla_valores)
+            print(saida)
+        else:
+            check_tamanho()
+
     if len(entrada) == 5:
         tupla_valores = tuple(entrada)
+        checagem = check_numeros(tupla_valores)
+        if checagem == True:
+            saida = transforma_inteiro(tupla_valores)
+            print(saida)
+        else:
+            check_tamanho()
 
-print(tupla_valores)
+check_tamanho()
 
 
-for item in saida:
-    if item > 99:
-        print("Entrada inválida: todos os números devem estar no intervalo de 0 a 99. Tente novamente.")
-        entrada = input("Digite 5 números quaisquer entre 0 - 99, separados por virgula")
-        entrada = entrada.split(",")
-
-for item in saida:
-    if item < 0:
-        print("Entrada inválida: todos os números devem estar no intervalo de 0 a 99. Tente novamente.")
-        entrada = input("Digite 5 números quaisquer entre 0 - 99, separados por virgula")
-        entrada = entrada.split(",")
-
-print(f"A entrada é{saida}")
+#aqui vemos que o programa retorna o que foi pedido no projeto, porém o código está confuso e bastante acoplado
+#a necessidade de fazer toda a lógica usando funções me fez deduzir que esse projeto seria resolvido de forma mais simples
+#utilizando programação orientada a objeto. partimos para o próximo passo do projeto.
