356acf2c12833d5a13c0d3d541288d2ac45f4a42
Diêgo Canafistula <diego.canafistula@gmail.com>
2025-11-25 12:28:54 -0300

    enunciado projeto 2

---
+++ miniprojetos/proj02.py
@@ -0,0 +1,6 @@
+#Crie um sistema simples de gerenciamento de hotel, composto das seguintes funcionalidades: 
+#cadastro de hospede, inclusião de tipo de quarto, serviço de alimentação, serviço de lavanderia, serviços adicionais
+# de modo a gerar um cadastro com as informações dos serviços contratados, retornando o valor agregado total
+# salvando essas informações em um banco de dados. 
+#o sistema deve permitir a manipulação livre de inclusão e remoção de serviços, assim como a exibição da base de dados 
+#de jospedes contendo todas informações relevantes.
